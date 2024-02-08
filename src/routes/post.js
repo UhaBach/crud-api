908b155f4442a41e7064af17068b8b7b6638b794
VelderGoose <vlniiv@mail.ru>
2024-02-08 22:22:07 +0700

    fix: corrected post request

--- src/routes/post.js
+++ src/routes/post.js
@@ -4,17 +4,20 @@ import { parse } from "querystring";
 export function createUser(req, res){
     let body = "";
     req.on("data", chunk => {
-        body += chunk.toString();
+        body = chunk.toString();
     });
-    req.on("end", () => {});
-    body = parse(body);
-    if (!Object.hasOnw(body, "username") || !Object.hasOnw(body, "age") || !Object.hasOnw(body, "hobbies")) {
-        res.statusCode = 400;
-        res.end("The request body does not contain one or more required fields (username, age, hobbies)." + 
-            "\nCheck the spelling of the request body and try again.");
-        return;
-    }
-    let user = storage.createUser(body.username, body.age, body.hobbies);
-    res.statusCode = 201;
-    res.end(JSON.stringify(user));
+    req.on("end", () => {
+        body = parse(body);
+        console.log(body);
+        if (!Object.hasOwn(body, "username") || !Object.hasOwn(body, "age") || !Object.hasOwn(body, "hobbies")) {
+            res.statusCode = 400;
+            res.end("The request body does not contain one or more required fields (username, age, hobbies)." + 
+                "\nCheck the spelling of the request body and try again.");
+            return;
+        }
+        let user = storage.createUser(body.username, body.age, body.hobbies);
+        res.statusCode = 201;
+        res.end(JSON.stringify(user));
+    });
+    
 }
